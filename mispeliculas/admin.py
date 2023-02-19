4a4805ef8ba55a7f97c678a03b8821400c7ef524
Marcela Gimenez <marcelasilvanagimenez@gmail.com>
2023-02-19 08:09:40 -0300

    pelis v1.0 - funcionando

---
+++ mispeliculas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from.models import Peliculas,Serie,Capitulo
+# Register your models here.
+admin.site.register(Peliculas)
+admin.site.register(Serie)
+admin.site.register(Capitulo)
---
+++ mispeliculas/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+class Peliculas(models.Model):
+    titulo=models.CharField(max_length=200)
+    genero=models.CharField(max_length=200)
+    director=models.CharField(max_length=200, blank=True)
+    fecha=models.DateField
+    descargada=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
+    
+class Serie(models.Model):
+    titulo=models.CharField(max_length=200)
+    genero=models.CharField(max_length=200)
+    fecha=models.DateField
+    temporadas=models.IntegerField(default=1)
+    completada=models.BooleanField(default=False)
+    descargada=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
+
+class Capitulo(models.Model):
+    numero=models.IntegerField(default=0)
+    temporada=models.IntegerField(default=1)
+    serie=models.ForeignKey(Serie,on_delete=models.CASCADE)
+    fecha_emision=models.DateField
+   
+    def __str__(self):
+        return self.serie.titulo + "" + self.temporada + "x" + self.numero
+        
+        """pelicula = Peliculas(titulo='Rocky',director='Silvester Stallone')
+        pelicula.save()
+        lista_peliculas=Peliculas.object.all()
+        print(lista_pelicula)
+
+        pelicula = Peliculas.object.get(titulo='Rocky')
+        print(pelicula.titulo)
+
+        pelicula = Peliculas.object.get(titulo='Titanic')
+        pelicula.director = 'otro'
+        pelicula.save()"""
