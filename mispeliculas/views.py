3fb28119b351c08fe3d16cd50ee73384a81a7cbd
Marcela Gimenez <marcelasilvanagimenez@gmail.com>
2023-02-20 19:31:17 -0300

    nueva mod

--- mispeliculas/views.py
+++ mispeliculas/views.py
@@ -5,15 +5,19 @@ from mispeliculas.models import *
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("Bienvenidos")
+    return render(request,'mispeliculas/inicio.html')
+    #return HttpResponse("Bienvenidos")
 
 def peli(request):
+    #return render(request,'mispeliculas/pelis.html')
     return HttpResponse("Vista Peliculas")
 
 def serie(request):
-    return HttpResponse("Vista Series")
+    return render(request,'mispeliculas/serie.html')
+    #return HttpResponse("Vista Series")
 
 def chapter(request):
-    return HttpResponse("Vista Capitulos")
+    return render(request,'mispeliculas/chapter.html')
+    #return HttpResponse("Vista Capitulos")
 
 
