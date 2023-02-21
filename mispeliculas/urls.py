78ff9eaf1ad9b116eb7c2b3e9e1db51d8a6f76cc
Marcela Gimenez <marcelasilvanagimenez@gmail.com>
2023-02-21 03:24:13 -0300

    mod 21feb version 3

--- mispeliculas/urls.py
+++ mispeliculas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from mispeliculas import views
 
 urlpatterns = [
-    path('',views.inicio),
+    path('',views.inicio, name="Inicio"),
     path('pelis',views.pelis,name= "Peliculas"),
     path('serie',views.serie,name= "Series"),
     path('chapter',views.chapter,name= "Capitulos"),
